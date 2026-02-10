c1c91acb46f2170ab65f38aece656a36fb6224ec
unknown <ardanuw@gmail.com>
2026-02-10 15:24:39 +0700

    make contact variable and console log

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let contacts = [
+    {
+        id:1,
+        fullName: "Ardanu W",
+        phone: "085780570715",
+        email: "ardanuw@gmail.com",
+        location: "Jakarta"
+    }
+]
+
+console.log(contacts)
