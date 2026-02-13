a32bdee6d985f1e6940989571c90c3c40d8a7bfc
unknown <ardanuw@gmail.com>
2026-02-13 18:03:09 +0700

    create formatting contact function

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "Ardanu W",
+    phone: "085780570715",
+    email: "ardanuw@gmail.com",
+    location: "Jakarta",
+  },
+  {
+    id: 2,
+    fullName: "Haidar H",
+    phone: "081234567890",
+    email: "mhaidarhanif@gmail.com",
+    location: "Jakarta",
+  },
+  {
+    id: 3,
+    fullName: "Ben N",
+    phone: "08987654321",
+    email: "bentinata@gmail.com",
+    location: "Kediri",
+  }
+];
 
-let contacts = [
-    {
-        id:1,
-        fullName: "Ardanu W",
-        phone: "085780570715",
-        email: "ardanuw@gmail.com",
-        location: "Jakarta"
-    }
-]
+function formatContact(contact) {
+  for (let i = 0; i < contacts.length; i++) {
+    console.log(
+      `${i+1}. ${contacts[i].fullName}, ${contacts[i].phone}, ${contacts[i].email}, in ${contacts[i].location}`,
+    );
+  }
+}
 
-console.log(contacts)
+formatContact(contacts)
